4858430913a1a602e1d49f06b2c8f575f71dff24
Mahdi Tehrani <mahdi@frequencydesign.com>
2016-10-05 15:33:40 -0700

    testing and need another branch. Stash wasn't working

--- js/controllers/superbowlController.js
+++ js/controllers/superbowlController.js
@@ -2,10 +2,10 @@ angular.module("anthology")
 
 .controller("superbowlController", ["$scope", "superbowlDataService", function($scope, superbowlDataService) {
 
-	$scope.isVisible = true;
-	$scope.showHide = function() {
+	//$scope.isVisible = true;
+/*	$scope.showHide = function() {
 		$scope.isVisible = $scope.isVisible ? false : true;
-	};
+	};*/
 
 	$scope.superbowlData = superbowlDataService.GetSuperbowlData();
 
--- js/directives/directives.js
+++ js/directives/directives.js
@@ -36,27 +36,46 @@ angular.module("anthology")
 	}
 })
 .directive("gameArticle", function() {
+	var theIsVisible = true;
 	return {
 		restrict: "E",
-		templateUrl: "js/directives/gameArticle.html",
+		templateUrl: "js/directives/gameArticle.html?4",
 		replace: true,
 		scope: {
 			title: "@",
 			text: "=",
 			isVisible: "=",
 			showHide: "&"
+		},
+		link: function(scope, element, attrs) {
+			//var isVisible = scope.isVisible;
+/*			var showHide = function() {
+				scope.isVisible = scope.isVisible ? false : true;
+			};*/
+			scope.isVisible = theIsVisible;
+			scope.showHide = function() {
+				scope.isVisible = scope.isVisible ? false : true;
+			};
+			console.log(scope);
+			console.log(element);
+			console.log(scope.isVisible);
+			console.log(scope.showHide);
 		}
 	}
 })
-.directive("gameArticleContent", function() {
+/*.directive("gameArticleContent", function() {
 	return {
 		restrict: "E",
-		templateUrl: "js/directives/gameArticleContent.html",
+		templateUrl: "js/directives/gameArticleContent.html?2",
 		replace: true,
 		scope: {
 			text: "=",
 			isVisible: "=",
 			showHide: "&"
+		},
+		link: function(scope, element, attrs) {
+		
+
 		}
 	}
-})
+})*/
